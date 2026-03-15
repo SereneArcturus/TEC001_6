735577e1e49fcc0629651547acc9639a6ded27ad
SereneArcturus <caonguyentuanminh279@gmail.com>
2026-03-15 21:28:25 +0700

    assignment 6

---
+++ Ex1.py
@@ -0,0 +1,14 @@
+numbers = []
+while True:
+    input_from_user = input("Type the number here - If you want to quit, just press Enter: ")
+    if input_from_user == "":
+        break
+    try:
+        num = float(input_from_user)
+        numbers.append(num)
+    except ValueError:
+        print("Wrong input, you just need to type a valid number:")
+numbers.sort(reverse=True) 
+print("These are 5 greatest numbers from your list:")
+for i in numbers[:5]:  
+    print(i)
